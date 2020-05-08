d46174cf1b05ff4a8b7bb885f3d9553d716b5e3e
max980213 <49967356+max980213@users.noreply.github.com>
2020-05-08 19:06:10 +0800

    Update wechatautoreply.py

--- wechatautoreply.py
+++ wechatautoreply.py
@@ -3,15 +3,35 @@ import json
 import requests
 import datetime
 
-bot=Bot(cache_path=True)  # 登录微信，括号里是缓存，短时间内不用每次运行都登录
+bot=Bot(cache_path=True)  # 登录微信，括号里是缓存，不用每次运行都登录
+
+friends=bot.friends()
+friendslist=[]
+for one in friends:
+    friendslist.append(one.name)
+
+# 创建还有列表
+'''
+for one in friendslist:
+    try:
+        print(one)
+    except:
+        print('no')
+        continue
+'''
 
 @bot.register()
 
 def recv_send_msg(recv_msg):
+    print(recv_msg.sender)
+    if recv_msg.sender.name in friendslist:
+        #print('收到了来自 %s 的消息，他说： %s ' % recv_msg.sender,recv_msg.text)
         print('收到的消息：',recv_msg.text)
         now_time=datetime.datetime.now()
         string='现在是： '+str(now_time)
-        return '我是正在测试的自动回复，'+string  # 该位置是自动回复的内容
+        return '：我是正在测试的自动回复，'+string
+    else:
+        pass
 
 embed()
 
